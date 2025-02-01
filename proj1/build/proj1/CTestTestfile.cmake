f2a3c57cab2d27fc1afb62db6b0e58f58b505438
Andrew-Ji10 <andrewji@berkeley.edu>
2025-01-31 22:28:54 -0800

    line and circle, polygon work in progress

---
+++ proj1/build/proj1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106b/sp25/class/ee106b-aaf/ros_workspaces/proj1/src/proj1
+# Build directory: /home/cc/ee106b/sp25/class/ee106b-aaf/ros_workspaces/proj1/build/proj1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ proj1/build/usb_cam/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106b/sp25/class/ee106b-aaf/ros_workspaces/proj1/src/usb_cam
+# Build directory: /home/cc/ee106b/sp25/class/ee106b-aaf/ros_workspaces/proj1/build/usb_cam
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
